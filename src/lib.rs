fbcc93096d7b91f77010069cb42ab0d535064a7b
Wenping Guo <ybyygu@gmail.com>
2020-01-04 09:49:30 +0800

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+// lib.rs
+// :PROPERTIES:
+// :header-args: :tangle src/lib.rs
+// :END:
+
+// [[file:~/Workspace/Programming/gosh-rs/core/gosh-core.note::*lib.rs][lib.rs:1]]
+pub use gchemol;
+pub use guts;
+pub use vecfx;
+// lib.rs:1 ends here
