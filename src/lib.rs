9dca408c56254305647727b38584a780c05e4eec
Wenping Guo <ybyygu@gmail.com>
2020-01-04 11:00:18 +0800

    update cargo.toml

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@
 
 // [[file:~/Workspace/Programming/gosh-rs/core/gosh-core.note::*lib.rs][lib.rs:1]]
 pub use gchemol;
-pub use guts;
+pub use gut;
+pub use text_parser;
 pub use vecfx;
 // lib.rs:1 ends here
